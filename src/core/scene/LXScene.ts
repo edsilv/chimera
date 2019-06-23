a365291a5193b59f74d82b58bb33da597494ffac
Edward Silverton <e.silverton@gmail.com>
2019-06-23 17:43:07 +0100

    update loop

--- src/core/scene/LXScene.ts
+++ src/core/scene/LXScene.ts
@@ -1,16 +1,29 @@
-import { Clock } from "three/build/three.module";
+import { World } from "ecsy";
 import { LXEntity } from "../LXEntity";
+import { bind } from "../../utils/bind";
 
 export class LXScene extends LXEntity {
+  private _world: World;
+  private _prevTime: number;
+
   constructor(self) {
     self = super(self);
-    self.clock = new Clock();
+    self._world = new World();
+    self._prevTime = performance.now();
+    self.update = bind(self.update, self);
+    requestAnimationFrame(self.update);
     console.log("LXScene constructed");
     return self;
   }
 
   connectedCallback() {
     console.log("LXScene connected");
+  }
+
+  update(time: number) {
+    const dt: number = time - this._prevTime;
+    this._world.execute(dt, time);
+    requestAnimationFrame(this.update);
   }
 }
 
